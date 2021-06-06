38d5bbba00de622cd960cce75c09e1c64d9bb6ef
Yusuf <priler001@gamil.com>
2021-06-06 21:24:38 +0600

    commit two

--- js/main.js
+++ js/main.js
@@ -8,3 +8,10 @@ buttons.forEach(item => {
     item.classList.add('active')
 })
 })
+
+$(document).ready(function() {
+    $('.menu-burger__header').click(function(){
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.navbar-ul').toggleClass('open-menu');
+    });
+});
